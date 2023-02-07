3c8da35aee800b0d80cb5c773ee38332c3e70f8d
erkaevrus <erk_08@mail.ru>
2023-02-07 12:40:23 +0500

    complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const openBrackets = []
+  const closeBrackets = []
+  const line = []
+
+  for (elem of bracketsConfig) {
+      openBrackets.push(elem[0])
+      closeBrackets.push(elem[1])
+  }
+
+  for (let bracket of str) {
+      if (closeBrackets.includes(bracket) && line.length > 0 && closeBrackets.indexOf(bracket) === openBrackets.indexOf(line[line.length -1])) {
+          line.pop()
+      } else if (openBrackets.includes(bracket)) {
+          line.push(bracket)
+      } else {
+          return false
+      }
+  }
+  return line.length === 0
 }
